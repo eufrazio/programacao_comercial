6a77c74dc18bf8e92366c89e1f3d419c1b018c3c
eufrazio <eufrazius@gmail.com>
2016-10-10 21:28:17 -0300

    Inicio da Tela Inicial

--- virtual/anamnese/medicos/urls.py
+++ virtual/anamnese/medicos/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from medicos import views
 
 urlpatterns = [
-   url(r'^$', views.MedicosList.as_view(), name='listar-medicos'),
+   url(r'^listar/$', views.MedicosList.as_view(), name='listar-medicos'),
    url(r'^novo/$', views.MedicosNew.as_view(), name='novo-medico'),
-   url(r'^(?P<pk>[0-9]+)/$', views.MedicosEdit.as_view(), name='editar-medico'),
+   url(r'^editar/(?P<pk>[0-9]+)/$', views.MedicosEdit.as_view(), name='editar-medico'),
    url(r'^excluir/(?P<pk>[0-9]+)/$', views.MedicosDelete.as_view(), name='deletar-medico'),
 ]
