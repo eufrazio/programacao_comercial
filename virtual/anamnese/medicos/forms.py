197d3b7c58ca727eb810da33ec2185a56eabbfd4
eufrazio <eufrazius@gmail.com>
2016-11-01 21:38:03 -0300

    Modelamos formulários(forms)

--- virtual/anamnese/medicos/forms.py
+++ virtual/anamnese/medicos/forms.py
@@ -10,3 +10,15 @@ class FormularioMedico(forms.ModelForm):
   class Meta:
     model = Medico
     exclude = []
+
+
+  def __init__(self, *args, **kwargs):
+    super(FormularioMedico, self).__init__(*args, **kwargs)
+    self.fields['nome'].widget.attrs['placeholder'] = 'Nome'
+    self.fields['nome'].widget.attrs['size'] = '100'
+    self.fields['crm'].widget.attrs['placeholder'] = 'CRM'
+    self.fields['especialidade'].widget.attrs['placeholder'] = 'Especialidade'
+    self.fields['especialidade'].widget.attrs['size'] = '32'
+    self.fields['telefone'].widget.attrs['placeholder'] = 'Telefone'
+    self.fields['areamedica'].widget.attrs['placeholder'] = 'Area Médica'
+
