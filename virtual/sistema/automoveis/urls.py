8598cccb4ba590c38f0f27f6f09a527584cc9102
eufrazio <eufrazius@gmail.com>
2017-03-30 22:18:07 -0300

    Criacao do projeto systematas

--- virtual/sistema/automoveis/urls.py
+++ virtual/sistema/automoveis/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from automoveis import views
 
 urlpatterns = [
-	url(r'^listar/$', views.AutomoveisList.as_view(), name='listar-automoveis'),
+	url(r'^$', views.AutomoveisList.as_view(), name='listar-automoveis'),
 	url(r'^novo/$', views.AutomoveisNew.as_view(), name='novo-automovel'),
 	url(r'^(?P<pk>[0-9]+)/$', views.AutomoveisEdit.as_view(), name='editar-automovel'),
 	url(r'^excluir/(?P<pk>[0-9]+)/$', views.AutomoveisDelete.as_view(), name='deletar-automovel'),
